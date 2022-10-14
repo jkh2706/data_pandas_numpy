314d0cb86c5025ce44c309e18065e213ba117f71
Jengine <jihoopapa0504@gmail.com>
2022-10-14 22:24:21 +0900

    studying pandas

---
+++ 1_array.py
@@ -0,0 +1,24 @@
+# numpy 로 배열 만들기
+
+import numpy as np
+
+# d = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])  # 3행 4열
+# print(d)
+# print(d[1,2])
+# print(d[1][2])
+# d1 = d[1:,2:]
+# print(d1)
+
+# data 크기 및 유형 확인
+# d = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+# print(d.shape)
+# print(d)
+# print(d.dtype)
+
+# data 타입 변경하기
+# d = np.arange(1,5) # 1~4 까지의 정수를 배열로 만든다.
+# print(d)
+# d1 = d.astype(float)  # chnage int tpye data to float
+# print(d1)
+# d2 = d1.astype(int)  # chane float type data to int 
+# print(d2)
---
+++ 2_pandas.py
@@ -0,0 +1,3 @@
+# pandas 롤 데이터 프레임을 이용해보자
+
+import pandas as pd
